61bd3872cbf47747a8c284a97b7a379fbf7950bb
Vntr Admin <admin@vntr.io>
2016-09-21 21:54:03 -0500

    Ja-be-ja: Fleshing out the manager test

--- src/test/java/io/vntr/jabeja/JabejaManagerTest.java
+++ src/test/java/io/vntr/jabeja/JabejaManagerTest.java
@@ -1,6 +1,9 @@
 package io.vntr.jabeja;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.*;
 
 /**
  * Created by robertlindquist on 9/20/16.
@@ -9,10 +12,59 @@ public class JabejaManagerTest {
 
     @Test
     public void testGetRandomSamplingOfUsers() {
-        //TODO: do this
+        int numUsers = 20;
+        int numRuns = 100000;
+        double alpha = 1.1D;
+        JabejaManager manager = new JabejaManager(alpha, 2D, .2D, 9);
+        Long pid1 = manager.addPartition();
+        Long pid2 = manager.addPartition();
+
+        for(long id=1; id<=numUsers; id++) {
+            manager.addUser(new JabejaUser("User " + id,  id,  id%2 == 1 ? pid1 : pid2, alpha, manager));
+        }
+
+        int[] count = new int[numUsers];
+        int totalCount = 0;
+        for(int i=0; i<numRuns; i++) {
+            int howMany = (int)(Math.random() * numUsers);
+            Collection<JabejaUser> users = manager.getRandomSamplingOfUsers(howMany);
+            totalCount += howMany;
+            for(JabejaUser user : users) {
+                int intId = user.getId().intValue() - 1;
+                count[intId]++;
+            }
+        }
+
+        double averageCountPerUser = ((double) totalCount) / numUsers;
+        double threshold = 0.9 * averageCountPerUser;
+        for(int i=0; i<numUsers; i++) {
+            Assert.assertTrue(count[i] > threshold);
+        }
     }
+
+
     @Test
     public void testRepartition() {
+        int numUsers = 1000;
+        double alpha = 1.1D;
+        JabejaManager manager = new JabejaManager(alpha, 2D, .2D, 9);
+        Long pid1 = manager.addPartition();
+        Long pid2 = manager.addPartition();
+
+        for(long id=1; id<=numUsers; id++) {
+            manager.addUser(new JabejaUser("User " + id,  id,  id%2 == 1 ? pid1 : pid2, alpha, manager));
+        }
+
+
+        for(long id=1; id<=numUsers; id++) {
+            int numFriends = (int)(Math.random() * numUsers * 0.03);
+            Collection<JabejaUser> friends = manager.getRandomSamplingOfUsers(numFriends);
+            for(JabejaUser friend : friends) {
+                manager.befriend(id, friend.getId());
+            }
+        }
+
+        manager.repartition();
         //TODO: do this
     }
 
