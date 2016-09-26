b9b91d3a340401c6cbb4aee5daa9337460a53bc4
Vntr Admin <admin@vntr.io>
2016-09-25 23:18:18 -0500

    Added newer, simpler graph initialization method to SPAR test utils.

--- src/test/java/io/vntr/spar/SparTestUtils.java
+++ src/test/java/io/vntr/spar/SparTestUtils.java
@@ -253,4 +253,60 @@ public class SparTestUtils {
 
         return partitionIdsWithNMasters;
     }
+
+    public static SparManager initGraph(int minNumReplicas, Map<Long, Set<Long>> partitions, Map<Long, Set<Long>> friendships, Map<Long, Set<Long>> replicaPartitions) {
+        SparManager manager = new SparManager(minNumReplicas);
+        for(Long pid : partitions.keySet()) {
+            manager.addPartition(pid);
+        }
+
+        Map<Long, Long> uToMasterMap = getUToMasterMap(partitions);
+        Map<Long, Set<Long>> uToReplicasMap = getUToReplicasMap(replicaPartitions, uToMasterMap.keySet());
+
+        for(Long uid : friendships.keySet()) {
+            SparUser user = new SparUser("User " + uid, uid);
+            Long pid = uToMasterMap.get(uid);
+            user.setMasterPartitionId(pid);
+            user.setPartitionId(pid);
+
+            manager.addUser(user, pid);
+
+            for (Long rPid : uToReplicasMap.get(uid)) {
+                manager.addReplica(user, rPid);
+            }
+        }
+
+        for(Long uid : friendships.keySet()) {
+            for(Long friendId : friendships.get(uid)) {
+                manager.befriend(manager.getUserMasterById(uid), manager.getUserMasterById(friendId));
+            }
+        }
+
+        return manager;
+    }
+
+    private static Map<Long, Long> getUToMasterMap(Map<Long, Set<Long>> partitions) {
+        Map<Long, Long> map = new HashMap<Long, Long>();
+        for(Long pid : partitions.keySet()) {
+            for(Long uid : partitions.get(pid)) {
+                map.put(uid, pid);
+            }
+        }
+        return map;
+    }
+
+    private static Map<Long, Set<Long>> getUToReplicasMap(Map<Long, Set<Long>> replicaPartitions, Set<Long> allUids) {
+        Map<Long, Set<Long>> map = new HashMap<Long, Set<Long>>();
+        for(Long uid : allUids) {
+            map.put(uid, new HashSet<Long>());
+        }
+        for(Long pid : replicaPartitions.keySet()) {
+            for(Long uid : replicaPartitions.get(pid)) {
+                map.get(uid).add(pid);
+            }
+        }
+        return map;
+    }
+
+
 }
