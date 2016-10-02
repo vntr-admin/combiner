d79a0240fc118db55b17b79dcb5819dceb3a7f24
Vntr Admin <admin@vntr.io>
2016-10-02 13:23:31 -0500

    Adding method to generate a valid set of replica locations given k-redundancy constraints

--- src/test/java/io/vntr/TestUtils.java
+++ src/test/java/io/vntr/TestUtils.java
@@ -1,9 +1,8 @@
 package io.vntr;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import io.vntr.utils.ProbabilityUtils;
+
+import java.util.*;
 
 /**
  * Created by robertlindquist on 9/24/16.
@@ -59,5 +58,54 @@ public class TestUtils {
         }
 
         return userIdToFriendIds;
+    }
+
+    public static Map<Long, Set<Long>> getInitialReplicasObeyingKReplication(int minNumReplicas, Map<Long, Set<Long>> partitions, Map<Long, Set<Long>> friendships) {
+        Map<Long, Set<Long>> replicas = new HashMap<Long, Set<Long>>();
+        for(Long pid : partitions.keySet()) {
+            replicas.put(pid, new HashSet<Long>());
+        }
+
+        Map<Long, Set<Long>> replicaLocations = new HashMap<Long, Set<Long>>();
+        for(Long uid : friendships.keySet()) {
+            replicaLocations.put(uid, new HashSet<Long>());
+        }
+
+        Map<Long, Long> uMap = new HashMap<Long, Long>();
+        for(Long pid : partitions.keySet()) {
+            for(Long uid : partitions.get(pid)) {
+                uMap.put(uid, pid);
+            }
+        }
+
+        //Step 1: add replicas for friends in different partitions
+        for(Long uid1 : friendships.keySet()) {
+            for(Long uid2 : friendships.get(uid1)) {
+                Long pid1 = uMap.get(uid1);
+                Long pid2 = uMap.get(uid2);
+                if(!pid1.equals(pid2)) {
+                    replicas.get(pid1).add(uid2);
+                    replicas.get(pid2).add(uid1);
+                    replicaLocations.get(uid1).add(pid2);
+                    replicaLocations.get(uid2).add(pid1);
+                }
+            }
+        }
+
+        //Step 2: add replicas as necessary for k-replication
+        for(Long uid : replicaLocations.keySet()) {
+            int numShort = minNumReplicas - replicaLocations.get(uid).size();
+            if(numShort > 0) {
+                Set<Long> possibilities = new HashSet<Long>(partitions.keySet());
+                possibilities.removeAll(replicaLocations.get(uid));
+                possibilities.remove(uMap.get(uid));
+                Set<Long> newReplicas = ProbabilityUtils.getKDistinctValuesFromList(numShort, new LinkedList<Long>(possibilities));
+                for(Long pid : newReplicas) {
+                    replicas.get(pid).add(uid);
+                }
+            }
+        }
+
+        return replicas;
     }
 }
