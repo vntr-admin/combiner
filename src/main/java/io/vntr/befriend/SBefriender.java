182b83b6bee63f0251374f007bda6793e31aa497
Vntr Admin <admin@vntr.io>
2017-05-07 15:57:05 -0500

    Subtle fix to bring SBefriender into alignment with original SPAR paper

--- src/main/java/io/vntr/befriend/SBefriender.java
+++ src/main/java/io/vntr/befriend/SBefriender.java
@@ -40,7 +40,7 @@ public class SBefriender {
                 return LARGE_TO_SMALL;
             }
 
-            float imbalanceRatio = (smallerMasters + 1f) / largerMasters;
+            float imbalanceRatio = smallerMasters / largerMasters;
             float ratioOfSecondBestToBest = ((float) Math.min(stay, toLarger)) / toSmaller;
             if (ratioOfSecondBestToBest > imbalanceRatio) {
                 return LARGE_TO_SMALL;
@@ -52,7 +52,7 @@ public class SBefriender {
                 return SMALL_TO_LARGE;
             }
 
-            float imbalanceRatio = (largerMasters + 1f) / smallerMasters;
+            float imbalanceRatio = largerMasters / smallerMasters;
             float ratioOfSecondBestToBest = ((float) Math.min(stay, toSmaller)) / toLarger;
             if (ratioOfSecondBestToBest > imbalanceRatio) {
                 return SMALL_TO_LARGE;
